da5fd5fa008df2eb1b9a40cb3eec48660db73da5
RamiroRendon <deadspartan49@gmail.com>
2020-10-17 16:26:57 -0500

    Chidori Enrutado

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeadbarComponent } from './headbar/headbar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    HeadbarComponent,
+    BreadcrumbsComponent,],
+    exports: [
+      SidebarComponent,
+      HeadbarComponent,
+      BreadcrumbsComponent,],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
