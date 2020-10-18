c1ca6d250154b8b8f9f6e74be3472f12a6931116
RamiroRendon <deadspartan49@gmail.com>
2020-10-17 19:07:07 -0500

    progress component chidori

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,8 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -22,6 +24,6 @@ import { RouterModule } from '@angular/router';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule, FormsModule, ComponentsModule],
 })
 export class PagesModule { }
